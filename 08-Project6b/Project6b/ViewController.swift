c68a60b081e62283a7f43e97ea56646257e2ae3b
clarknt <24315769+clarknt@users.noreply.github.com>
2019-05-14 13:33:55 -0400

    Project 6b

---
+++ 08-Project6b/Project6b/ViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = .red
+        label1.text = "THESE"
+        label1.sizeToFit()
+ 
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = .cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
+
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = .yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
+
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = .green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
+
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = .orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
+
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+        // 1.
+        // Auto Layout in code: addConstraints with Visual Format Language
+        // Auto Layout metrics and priorities: constraints(withVisualFormat:)
+        
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//
+//        for label in viewsDictionary.keys {
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//
+//        // v1
+//        //view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(==88)]-[label2(==88)]-[label3(==88)]-[label4(==88)]-[label5(==88)]-(>=10)-|", options: [], metrics: nil, views: viewsDictionary))
+//
+//
+//        let metrics = ["labelHeight": 88]
+//
+//        // v2
+//        //view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight)]-[label2(labelHeight)]-[label3(labelHeight)]-[label4(labelHeight)]-[label5(labelHeight)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
+//
+//        // v3
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+        
+        
+        // 2.
+        // Auto Layout anchors
+        
+        var previous: UILabel?
+        
+        for label in [label1, label2, label3, label4, label5] {
+            //label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            
+            //label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
+           
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            }
+            else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+
+            previous = label
+        }
+    }
+}
+
