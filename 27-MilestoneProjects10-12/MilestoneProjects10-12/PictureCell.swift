2755a0481ca7aa7cdb935336e31e4907bf032768
clarknt <24315769+clarknt@users.noreply.github.com>
2019-05-22 18:20:51 -0400

    Milestone projects 10-12

---
+++ 27-MilestoneProjects10-12/MilestoneProjects10-12/PictureCell.swift
@@ -0,0 +1,13 @@
+//
+//  PictureCellTableViewCell.swift
+//  MilestoneProjects10-12
+//
+
+import UIKit
+
+class PictureCell: UITableViewCell {
+
+    @IBOutlet var pictureView: UIImageView!
+    @IBOutlet var captionLabel: UILabel!
+    
+}
---
+++ 27-MilestoneProjects10-12/README.md
@@ -0,0 +1,11 @@
+# Milestone Projects 7-9
+
+https://www.hackingwithswift.com/100/50
+
+Bonus:
+- Swipe left to delete item
+
+## Screenshots
+
+![screenshot1](screenshots/screen01.png)
+![screenshot2](screenshots/screen02.png)
