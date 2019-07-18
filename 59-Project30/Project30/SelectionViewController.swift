6d1f1387d0ca9ef8781c8238f9e649c1718d713f
clarknt <24315769+clarknt@users.noreply.github.com>
2019-07-18 17:50:37 -0400

    Challenge 3

--- 59-Project30/Project30/SelectionViewController.swift
+++ 59-Project30/Project30/SelectionViewController.swift
@@ -12,7 +12,11 @@ class SelectionViewController: UITableViewController {
     var items = [String]() // this is the array that will store the filenames to load
     
     var dirty = false
-
+    
+    // challenge 3
+    var images: [UIImage?] = [UIImage]()
+    var finishedLoadingImages = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,9 +24,19 @@ class SelectionViewController: UITableViewController {
 
         tableView.rowHeight = 90
         tableView.separatorStyle = .none
+        
         // make sure a cell is returned by dequeueReusableCell
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         
+        // challenge 3
+        DispatchQueue.global().async { [weak self] in
+            self?.loadImages()
+        }
+    }
+    
+    // challenge 3
+    // run on background thread
+    func loadImages() {
         // load all the JPEGs into our array
         let fm = FileManager.default
 
@@ -32,12 +46,67 @@ class SelectionViewController: UITableViewController {
                 for item in tempItems {
                     if item.range(of: "Large") != nil {
                         items.append(item)
+                        // try to load from cache
+                        if let image = saveToCache(name: item) {
+                            images.append(image)
+                        }
+                        // or create and save to cache
+                        else {
+                            images.append(createThumbnail(currentImage: item))
+                        }
                     }
                 }
             }
         }
+        
+        finishedLoadingImages = true
+        tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+    }
+    
+    // challenge 3
+    func saveToCache(name: String) -> UIImage? {
+        let path = getDocumentsDirectory().appendingPathComponent(name)
+        return UIImage(contentsOfFile: path.path)
+    }
+    
+    // challenge 3
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 
+    // challenge 3
+    func createThumbnail(currentImage: String) -> UIImage? {
+        // find the image for this cell, and load its thumbnail
+        let imageRootName = currentImage.replacingOccurrences(of: "Large", with: "Thumb")
+        
+        // challenge 1
+        guard let path = Bundle.main.path(forResource: imageRootName, ofType: nil) else { return nil }
+        guard let original = UIImage(contentsOfFile: path) else { return nil }
+        
+        // scale down the image
+        let renderRect = CGRect(origin: .zero, size: CGSize(width: 90, height: 90))
+        let renderer = UIGraphicsImageRenderer(size: renderRect.size)
+        
+        let rounded = renderer.image { ctx in
+            ctx.cgContext.addEllipse(in: renderRect)
+            ctx.cgContext.clip()
+            
+            original.draw(in: renderRect)
+        }
+        
+        saveToCache(name: currentImage, image: rounded)
+        return rounded
+    }
+
+    // challenge 3
+    func saveToCache(name: String, image: UIImage) {
+        let imagePath = getDocumentsDirectory().appendingPathComponent(name)
+        if let pngData = image.pngData() {
+            try? pngData.write(to: imagePath)
+        }
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
@@ -55,6 +124,11 @@ class SelectionViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // challenge 3
+        if !finishedLoadingImages {
+            return 0
+        }
+        
         // Return the number of rows in the section.
         return items.count * 10
     }
@@ -63,40 +137,24 @@ class SelectionViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // reuse cells instead of creating a new one each time
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        //let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
 
-        // find the image for this cell, and load its thumbnail
-        let currentImage = items[indexPath.row % items.count]
-        let imageRootName = currentImage.replacingOccurrences(of: "Large", with: "Thumb")
-        
-        // challenge 1
-        guard let path = Bundle.main.path(forResource: imageRootName, ofType: nil) else { return cell }
-        guard let original = UIImage(contentsOfFile: path) else { return cell }
-
-        // scale down the image
+        // challenge 3
+        let index = indexPath.row % items.count
+        cell.imageView?.image = images[index]
         let renderRect = CGRect(origin: .zero, size: CGSize(width: 90, height: 90))
-        let renderer = UIGraphicsImageRenderer(size: renderRect.size)
-
-        let rounded = renderer.image { ctx in
-            ctx.cgContext.addEllipse(in: renderRect)
-            ctx.cgContext.clip()
-
-            original.draw(in: renderRect)
-        }
-
-        cell.imageView?.image = rounded
 
         // give the images a nice shadow to make them look a bit more dramatic
         cell.imageView?.layer.shadowColor = UIColor.black.cgColor
         cell.imageView?.layer.shadowOpacity = 1
         cell.imageView?.layer.shadowRadius = 10
         cell.imageView?.layer.shadowOffset = CGSize.zero
+        
         // force round shadow instead of letting iOS having to calculate itself
         cell.imageView?.layer.shadowPath = UIBezierPath(ovalIn: renderRect).cgPath
 
         // each image stores how often it's been tapped
         let defaults = UserDefaults.standard
-        cell.textLabel?.text = "\(defaults.integer(forKey: currentImage))"
+        cell.textLabel?.text = "\(defaults.integer(forKey: items[index]))"
 
         return cell
     }
--- 59-Project30/README.md
+++ 59-Project30/README.md
@@ -8,7 +8,7 @@ Includes solutions to the [challenges](https://www.hackingwithswift.com/read/30/
 
 1. Go through project 30 and remove all the force unwraps. Note: implicitly unwrapped optionals are not the same thing as force unwraps – you’re welcome to fix the implicitly unwrapped optionals too, but that’s a bonus task.
 2. [Challenge 2](../60-Project30-Challenge2)
-3. Coming soon
+3. For a tougher challenge, take the image generation code out of cellForRowAt: generate all images when the app first launches, and use those smaller versions instead. For bonus points, combine the getDocumentsDirectory() method I introduced in project 10 so that you save the resulting cache to make sure it never happens again.
 
 ## Screenshots
 
